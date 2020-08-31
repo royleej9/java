24c7ce5c794539c64a18b02c63f3721d8b28f341
royleej9 <royleej9@gmail.com>
2020-08-31 23:57:53 +0900

    추가 - jap 조회/추가/수정/삭제

--- jpa/src/main/java/royleej9/study/jpa/Application2.java
+++ jpa/src/main/java/royleej9/study/jpa/Application2.java
@@ -23,16 +23,48 @@ public class Application2 {
 		addUser(entityManagerFactory);
 		updateUser(entityManagerFactory);
 		Member member = findUser(entityManagerFactory);
-		
 		logger.info("findUser:: {}", member.toString());
+		
+		removeUser(entityManagerFactory);
+		
+		member = findUser(entityManagerFactory);
+		if (member != null) {
+			logger.info("findUser-after remove:: {}", member.toString());			
+		}
 
 		entityManagerFactory.close();
+	}
+
+	private static void removeUser(EntityManagerFactory entityManagerFactory) {
+		final EntityManager entityManager = entityManagerFactory.createEntityManager();
+		final EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		try {
+			// 등록/수정/삭제시 트랜잭션 필요
+			entityTransaction.begin();
+
+			String id = "id1";
+			// 삭제 대상이 영속성 컨텍스트에 저장되어 있어야함 - 영속 상태
+			Member findMember = entityManager.find(Member.class, id);
+			entityManager.remove(findMember);
+			
+			entityTransaction.commit();
+		} catch (Exception ex) {
+			// 롤백 -
+			entityTransaction.rollback();
+			ex.printStackTrace();
+		} finally {
+			// 6 manager 종료
+			entityManager.close();
+		}		
 	}
 
 	private static Member findUser(EntityManagerFactory entityManagerFactory) {
 		final EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 		String id = "id1";
+		// 1차 캐시에서 먼저 조회 -> 없을 경우 db에서 조회 
+		// 조회된 엔티티는 영속 상태가 된다
 		Member findMember = entityManager.find(Member.class, id);
 		entityManager.close();
 
@@ -44,6 +76,7 @@ public class Application2 {
 		final EntityTransaction entityTransaction = entityManager.getTransaction();
 
 		try {
+			// 등록/수정/삭제시 트랜잭션 필요
 			entityTransaction.begin();
 			
 			// 수정 entity 검색 - 영속성 컨텍스트에 없는 경우 db 조회
@@ -76,6 +109,7 @@ public class Application2 {
 		final EntityTransaction entityTransaction = entityManager.getTransaction();
 
 		try {
+			// 등록/수정/삭제시 트랜잭션 필요
 			entityTransaction.begin();
 
 			String id = "id1";
@@ -85,8 +119,11 @@ public class Application2 {
 			member.setAge(4);
 
 			// 등록
+			// 1차 캐시 entity 저장 / insert sql 생성 후 내부 sql 저장소에 보관
 			entityManager.persist(member);
-
+			
+			// 1.엔티티메니저의 flush 호출
+			// 2 동기화 작업 실행 ->내부 sql저장소에 보관된 sql을 db로 전달.실행 
 			entityTransaction.commit();
 		} catch (Exception ex) {
 			// 롤백 -
