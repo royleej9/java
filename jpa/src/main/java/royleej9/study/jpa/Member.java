35fc467b4838b27db8ae17baa5b9fb7fdd15e7c5
royleej9 <royleej9@gmail.com>
2020-09-04 00:21:02 +0900

    수정 - 엔티티 정리

--- jpa/src/main/java/royleej9/study/jpa/Member.java
+++ jpa/src/main/java/royleej9/study/jpa/Member.java
@@ -1,21 +1,55 @@
 package royleej9.study.jpa;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
+// 파마미터가 없는 public/protected 기본 생성자 필수
+// 생성자가 없는 경우 기본 생성자 자동 생성됨 - 자바 기본 기능
+// 파라미터가 있는 생성자를 만들 경우 기본 생성자를 직접 만들어야함
+// 저장 필드 final 사용 금지
+
+// 필수
 @Entity
-@Table(name = "Member")
+
+// 생략시 클래스 이름으로 테이블 매핑
+//@Table(name = "Member")
+@Table(name = "Member", uniqueConstraints = {
+		@UniqueConstraint(name = "NAME_AGE_UNIQUE", 
+					columnNames = { "NAME", "AGE" }) 
+		})
 public class Member {
 
 	@Id
 	@Column(name = "ID")
 	private String id;
 
-	@Column(name = "NAME")
+	// ddl 생성시 컬럼 제약 조건
+	// name, null 허용 여부, 길이
+	@Column(name = "NAME", nullable = false, length = 10)
 	private String userName;
 	private Integer age;
+
+	@Enumerated(EnumType.STRING)
+	private RoleType roleType;
+
+	@Temporal(TemporalType.TIMESTAMP) // 날짜 타입 매핑
+	private Date createdDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date lastModifiedDate;
+
+	@Lob
+	private String description;
 
 	public String getId() {
 		return id;
@@ -39,6 +73,38 @@ public class Member {
 
 	public void setAge(Integer age) {
 		this.age = age;
+	}
+
+	public RoleType getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(RoleType roleType) {
+		this.roleType = roleType;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	@Override
